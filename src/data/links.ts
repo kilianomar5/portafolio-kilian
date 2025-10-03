e7aacd9697f3351fc388b89354b47ca041633390
xXChicoGamerXX <148535887+kilianomar5@users.noreply.github.com>
2025-10-03 17:24:29 +0100

    Update links.ts

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,7 +2,7 @@ export const Links = {
   github: "https://github.com/kilianomar5",
   discord: "https://discord.com/users/722136781646200834", 
   email: "mailto:kilirguez@gmail.com",
-  resume: "https://www.dropbox.com/scl/fi/c6br0xs0nkgede42qvt16/Curr-culum-Kilian-Omar-Rodr-guez-D-az.pdf?rlkey=mzr09srcxpz2rkl7a9p2qhm0e&st=1z7v6n37&dl=1",
+  resume: "https://www.dropbox.com/scl/fi/c6br0xs0nkgede42qvt16/Curr-culum-Kilian-Omar-Rodr-guez-D-az.pdf?rlkey=mzr09srcxpz2rkl7a9p2qhm0e&st=n1xjtdb7&dl=1",
   emailonly: "kilirguez@gmail.com",
   location: "Santa Cruz de Tenerife, Islas Canarias, España",
   phone: "Envíame un mensaje y te responderé.",
